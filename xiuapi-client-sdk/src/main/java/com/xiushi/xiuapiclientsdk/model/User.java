526ab48fd0c13110af895ba0c54ed833fee6159e
Riaduhb <1501303212@qq.com>
2024-08-27 16:40:18 +0800

    发布/下线接口开发

---
+++ xiuapi-client-sdk/src/main/java/com/xiushi/xiuapiclientsdk/model/User.java
@@ -0,0 +1,12 @@
+package com.xiushi.xiuapiclientsdk.model;
+
+import lombok.Data;
+
+/**
+ * 用户
+ * @author DHB
+ */
+@Data
+public class User {
+    private String username;
+}
---
+++ xiuapi-interface/src/main/java/com/xiushi/xiuapiinterface/XiuapiInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.xiushi.xiuapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiuapiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiuapiInterfaceApplication.class, args);
+    }
+
+}
